106e03f586e95895909529daa22ea13bf4276c52
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-04-08 20:23:20 -0300

    feat: Adicionando validação dos campos

--- src/main/java/dev/rafael/itauJava10x/Transacao/TransacaoService.java
+++ src/main/java/dev/rafael/itauJava10x/Transacao/TransacaoService.java
@@ -8,4 +8,19 @@ import java.time.OffsetDateTime;
 @Service
 public class TransacaoService {
 
+	public void validarTransacao(TransacaoRequest transacaoRequest){
+
+		if (transacaoRequest.getValor() == null || transacaoRequest.getDataHora() == null) {
+			throw new IllegalArgumentException("Campos obrigatórios ausentes.");
+		}
+
+		if (transacaoRequest.getValor().compareTo(BigDecimal.ZERO) < 0 ){
+			throw new IllegalArgumentException("Erro: Isso não é uma transação válida. Apenas transações com valores positivos são válidos.");
+		}
+
+		if (transacaoRequest.getDataHora().isAfter(OffsetDateTime.now())){
+			throw new IllegalArgumentException("Erro: Isso não é uma transação válida. Apenas transações anteriores ao presente são válidas.");
+		}
+
+	}
 }
