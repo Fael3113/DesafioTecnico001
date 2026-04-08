c879d36e19d887ac9891ce8c0cc79fcbbf58d72b
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-04-08 18:13:22 -0300

    first commit

---
+++ src/test/java/dev/rafael/itauJava10x/ItauJava10xApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafael.itauJava10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauJava10xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
