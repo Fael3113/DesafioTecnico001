d1c502ac921f708f56fe31e942d123b084fb1830
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-04-08 19:45:21 -0300

    feat: Criando o modelo da Transacao e seu DTO

---
+++ src/main/java/dev/rafael/itauJava10x/Transacao/TransacaoService.java
@@ -0,0 +1,11 @@
+package dev.rafael.itauJava10x.Transacao;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Service
+public class TransacaoService {
+
+}
---
+++ src/main/java/dev/rafael/itauJava10x/Transacao/TransacoesController.java
@@ -0,0 +1,11 @@
+package dev.rafael.itauJava10x.Transacao;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacoesController {
+
+}
