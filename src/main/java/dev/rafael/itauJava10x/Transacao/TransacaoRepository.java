34fcb10382e9930bdaa7bface897dd5025883c6a
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-04-08 21:02:28 -0300

    feat: Adicionado o repositório, agora as requisições são salvas em uma lista em memória e com a possibilidade de excluir a lista

---
+++ src/main/java/dev/rafael/itauJava10x/Transacao/TransacaoRepository.java
@@ -0,0 +1,21 @@
+package dev.rafael.itauJava10x.Transacao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TransacaoRepository {
+
+	List<TransacaoRequest> transacaoLista = new ArrayList<>();
+
+	public void salvarDados(TransacaoRequest transacaoRequest) {
+		transacaoLista.add(transacaoRequest);
+	}
+
+	public void deletarDados(){
+		transacaoLista.clear();
+	}
+
+}
